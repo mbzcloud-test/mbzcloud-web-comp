099a623b4497196ea388b7f5d1e88dff5728021c
TrevorNathan <trevoratwijukire@gmail.com>
2022-10-05 11:02:26 +0300

    UPDATE: package version to v1.4.0

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export const greet = console.log(
-  'Hello world!, Greetings from mbzcloud-web-comp library.'
-);
+// named export
+export const greetings =
+  'Hello world!, Greetings from mbzcloud-web-comp library.';
+
+// default export
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+export default sum;
