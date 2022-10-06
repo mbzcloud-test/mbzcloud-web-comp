2c677b9cd6669e336f506567633904f36174de30
TrevorNathan <trevoratwijukire@gmail.com>
2022-10-06 12:56:37 +0300

    UPDATE: v1.8.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // named export
 export const greetings =
-  'Hello world!, Greetings from mbzcloud-web-comp github package v1.7.0.';
+  'Hello world!, Greetings from the latest mbzcloud-web-comp github package';
 
 // default export
 const sum = (a: number, b: number) => {
