c69ef1ab2448ca16070542f56ee46531d7a5ccaa
TrevorNathan <trevoratwijukire@gmail.com>
2022-10-11 21:07:14 +0300

    UPDATE: v2.5.0

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // named export
-export const greetings =
+const greetings =
   'Hello World!, Greetings from the latest mbzcloud-web-comp Github package.';
 
 // default export
 const sum = (a: number, b: number) => {
-  return 'The sum of two numbers is: ' + a + b;
+  return 'The sum of two numbers is: ' + (a + b);
 };
 
 const multiply = (a: number, b: number) => {
@@ -12,7 +12,7 @@ const multiply = (a: number, b: number) => {
 };
 
 const subtract = (a: number, b: number) => {
-  return 'The subtrsction of two numbers is: ' + a * b;
+  return 'The subtraction of two numbers is: ' + (a - b);
 };
 
-export default { sum, multiply, subtract };
+export default { greetings, sum, multiply, subtract };
